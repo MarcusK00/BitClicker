9e0fe3a115afa15cbf803e0aead886dc05a4a196
Marcus <marcuskristensen10@hotmail.com>
2025-07-08 10:11:26 +0200

    Added all the items.

---
+++ BlazeClick/BlazeClick/Models/ItemInformation.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazeClick.Models
+{
+    public enum ShopItemType { BitMiner, GPUFarm, DataCenter }
+
+    public class ShopItemInfo
+    {
+        public ShopItemType Type { get; }
+        public string Name { get; }
+        public int BaseCost { get; }
+        public int MiningPower { get; }
+        public int Owned { get; set; }
+        public int CurrentCost => (int)Math.Ceiling(BaseCost * Math.Pow(1.1, Owned)); // Example formula
+
+        public ShopItemInfo(ShopItemType type, string name, int baseCost, int miningPower)
+        {
+            Type = type;
+            Name = name;
+            BaseCost = baseCost;
+            MiningPower = miningPower;
+            Owned = 0;
+        }
+    }
+}
+
+
+
